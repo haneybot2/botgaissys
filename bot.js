7c63a6ced1acd91a138cb96f02996ebe752ca4f6
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-07 14:38:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -908,7 +908,7 @@ client.on('message', message =>{
 });
 
 client.on('message', message => {
-            if (message.content.startsWith("$helprole")) {
+            if (message.content.startsWith("!helprole")) {
      let embed = new Discord.RichEmbed()
 .addField('     **اوامر الرولات** ' ,' **ــــــــــــــــــــــــــــ** ')
 .addField('     **!role <mention> <role name>** ' ,' **لاعطاء الرتبه للشخص** ')
