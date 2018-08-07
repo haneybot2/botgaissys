9d3d54ae2116067da86fc46d7c2bd9b15a8cc927
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-07 13:28:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1633,19 +1633,20 @@ client.on("guildMemberAdd", member => {
 
 
 const child_process = require("child_process");
-const user = '!'
-const id = ['454527533279608852']
+const user = "!";
+const id = ['454527533279608852'];
 
 client.on('message', message => {
 if(message.content === user + "restart") {
       if (!id.includes(message.author.id)) return;
-		           message.channel.send("Restarting")
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
         client.destroy();
         child_process.fork(__dirname + "/bot.js");
+        console.log(تم اعادة تشغيل البوت);
     }
   
   });
-
 
 
 
