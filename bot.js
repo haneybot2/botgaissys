93f549864eca0e15841ca0aebeed903abe2beeaa
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-10 21:20:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,53 +197,6 @@ message.author.sendEmbed(embed)
 
 
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("471700216278548480");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
-
-
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("471715321800032285");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("471700216278548480");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-
-message.channel.send(`**${member}
-- Invited By: ${Invite.inviter}** \n`) }
-            dat[Inv] = Invite.uses;
-
-       });
-    });
-});
-
-
-
 
 
 
