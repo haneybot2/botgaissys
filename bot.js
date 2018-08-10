c3d581f79987e875c4ef694419b08c78fcb054f7
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-10 20:14:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,7 +197,45 @@ message.author.sendEmbed(embed)
 
 
 
-
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("471700216278548480");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("471715321800032285");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("471700216278548480");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 
@@ -806,7 +844,7 @@ if (message.member.voiceChannel == null) return message.channel.send(`**الرج
  
  //لمسح الشات
 client.on("message", message => {
-    var prefix = "-";
+    var prefix = "!";
  
             var args = message.content.substring(prefix.length).split(" ");
             if (message.content.startsWith("مسح")) {
@@ -851,6 +889,11 @@ client.on("message", message => {
 });
  
  
+
+
+
+
+
  
  
  //امر الممبر لعرض الناس
@@ -1588,7 +1631,27 @@ client.on('message', message => {
 
 
 
-
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', 'Muted'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | ممنوع نشر الروابط")
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500)
+   
+       
+    }
+    }
+})
+ 
+ 
 
 
 
