f11ce902a9a307aedaa899303213805705997925
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-11 11:12:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1711,23 +1711,12 @@ ${invite.url}
 
 
 
+// Your Avatar URL!
 client.on('message', message => {
-    if (message.content.startsWith("!avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
+    if (message.content === "!avatar") {
+    message.reply(message.author.avatarURL); 
     }
 });
-
 
 
 
