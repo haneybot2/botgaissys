e2ec4017555f84e9502bbe04e32248559a8deb46
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-14 14:35:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -181,7 +181,30 @@ message.author.sendEmbed(embed)
 
 
 
+  
+  
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+client.on('ready', () => {
+  wait(1000);
 
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const yumz = member.guild.channels.find("name", "a-guys");
+     yumz.send(`**Invited By : **<@${inviter.id}>`);
+   //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
+  
 
 
 
