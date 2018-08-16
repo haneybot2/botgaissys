3c0415403bba1e81bf39ff5390bb8b3395687e6b
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-16 12:54:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,10 @@ const client = new Discord.Client();
 const prefix = '!'
 const id = ['454527533279608852' , '416643173239226388' , '347788375018700802' , '374225808585130008'];
 
-const giphy = require('giphy-api')(); 
-const ms = require("ms");
-const fs = require('fs');
-let warns = JSON.parse(fs.readFileSync("./warnings.json"));;
-//id
-const Canvas = require("canvas");
-const jimp = require("jimp");
-//welcome
-const moment = require('moment');
+
+
+
+
 
 
 client.on('ready', () => {
