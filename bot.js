0eaea9b45178a1b3858adef6d83bc17bdfb63c67
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-19 02:03:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -969,7 +969,7 @@ client.on("message", message => {
 var AsciiTable = require('ascii-data-table').default
 client.on('message', message =>{
 
-    if(message.content == "!roles"){
+    if(message.content == "!allroles"){
         var 
         ros=message.guild.roles.size,
         data = [['Rank', 'RoleName']]
