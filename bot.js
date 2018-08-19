f473fff988ef24665373b7706dbf38683da04441
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-19 09:43:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1646,5 +1646,32 @@ if (message.content.startsWith(prefix + 'setavatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
+client.on('message', async message => {
+            if(!message.channel.guild) return;
+             if (message.content.startsWith("!status ")) {
+		     if (!id.includes(message.author.id)) return;
+let args = message.content.split(' ').slice(1).join(' ');
+            let sigMessage = await args;
+            
+            if (sigMessage === "online") {
+                client.user.setStatus("online");
+                message.author.send("Your status was set to online.");
+            }
+            if (sigMessage === "idle") {
+                client.user.setStatus("idle");
+                message.author.send("Your status was set to idle.");
+            }
+            if (sigMessage === "invisible") {
+                client.user.setStatus("invisible");
+                message.author.send("Your status was set to invisible.");
+            }
+            if (sigMessage === "dnd") {
+                client.user.setStatus("dnd");
+                message.author.send("Your status was set to dnd.");
+            }
+            // message.author.send("." + message.content);
+        
+}
+});
 
 client.login(process.env.BOT_TOKEN);
