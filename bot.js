e4da004e55af955045295a2c4c2060ce0dce6490
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-19 02:08:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -966,22 +966,6 @@ client.on("message", message => {
 		} 
 	} 
 });
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
-
-    if(message.content == "!allroles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
-
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
-});
 client.on('message', message => {
             if (message.content.startsWith( prefix + "helprole")) {
 		    if (!message.member.hasPermission('MANAGE_ROLES')) return ;
