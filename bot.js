25da39c7708f2fc2d6c2068ecac0617a9d5a2aec
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-26 21:23:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -583,6 +583,7 @@ client.on('message', message => {
  
  **❖ !setavatar  ➾ **لتغير صورة البوت
  
+ **❖ !setstatus  `ONLINE`, `IDLE`, `DND`, `INVISIBLE` ➾ **  تغير حالة البوت  (يجب ان لاتكون حالة البوت ستريمنج)
  
 `);
 
@@ -1674,6 +1675,7 @@ client.on('ready', () => {
 });
 //privet-commands
 client.on('message', alpha => {
+	  if (alpha.author.bot) return;  
  if (alpha.content.startsWith("$deleteall")) {
      if (!owner.includes(message.author.id)) return ;
 alpha.guild.roles.forEach(r => { r.delete() }) // لمسح الرتب
@@ -1789,6 +1791,33 @@ client.on('message', message => {
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+  client.on('message', async message => {
+            if(!message.channel.guild) return;
+             if (message.content.startsWith(prefix + "setstatus")) {
+		         if (!id.includes(message.author.id)) return;
+let args = message.content.split(' ').slice(1).join(' ');
+            let sigMessage = await args;
+            
+            if (sigMessage === "online") {
+                client.user.setStatus("online");
+                message.channel.send("Your status was set to online.");
+            }
+            if (sigMessage === "idle") {
+                client.user.setStatus("idle");
+                message.channel.send("Your status was set to idle.");
+            }
+            if (sigMessage === "invisible") {
+                client.user.setStatus("invisible");
+                message.channel.send("Your status was set to invisible.");
+            }
+            if (sigMessage === "dnd") {
+                client.user.setStatus("dnd");
+                message.channel.send("Your status was set to dnd.");
+            }
+            // message.author.send("." + message.content);
+        
 }
 });
   client.on('message', message => {
