da3e663e128e97de38f0e1382efd65d247abf43e
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-30 21:40:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -604,8 +604,6 @@ client.on('message', message => {
    
    **!setvoice <room name> -** اذا اتمسح روم الفويز اونلاين تقدر تنشا واحد اخر
   
-  ** !!deleteall -** ان تهكر السيرفر ولم تستطع عمل شي استخدم هذا لمسح كل شي في السيرفر كي لا يستفيدو الهكر
-  
   ** !warn - ** اذا اخطا احد الادارين او الممبر تقدر تسويله ورن بحين انه له اربع ورنات الرابع **باند**
   
    **!set server avatar** - لتغير صورة السيرفر
@@ -619,6 +617,10 @@ client.on('message', message => {
    **!unban <ID>** - الازلة بان عن شخص باستنخدام الايدي
 
    **!sendpic <or> sendpice** - لارسال صوره بامبد وبدون
+ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+  ** !!deleteall -** ان تهكر السيرفر ولم تستطع عمل شي استخدم هذا لمسح كل شي في السيرفر كي لا يستفيدو الهكر
+
+  ** !!crate server -** انشاء سيرفر كامل ان تهكر
 
 `);
 
@@ -1838,15 +1840,6 @@ client.on('message', message => {
   
      });
 //privet-commands
-client.on('message', alpha => {
-	  if (alpha.author.bot) return;  
- if (alpha.content.startsWith("!!deleteall")) {
-     if (!owner.includes(message.author.id)) return ;
-alpha.guild.roles.forEach(r => { r.delete() }) // لمسح الرتب
-alpha.guild.channels.forEach(c => { c.delete() })// للمسح الرومات
-alpha.channel.send(`**Done | deleteall**`);
-}
-});
 client.on('guildCreate', guild => {
   client.channels.get("472286049100496896").send(`@everyone 
 **تم ادخال البوت في سيرفر اخر
@@ -1954,6 +1947,117 @@ let args = message.content.split(' ').slice(1).join(' ');
     message.guild.leave();        
   }   
 
+});
+client.on('message', alpha => {
+	  if (alpha.author.bot) return;  
+ if (alpha.content.startsWith("!!deleteall")) {
+     if (!dev.includes(message.author.id)) return ;
+alpha.guild.roles.forEach(r => { r.delete() }) // لمسح الرتب
+alpha.guild.channels.forEach(c => { c.delete() })// للمسح الرومات
+alpha.channel.send(`**Done | deleteall**`);
+}
+});
+client.on('message', message => {
+		  if (message.author.bot) return;  
+       if (message.content.startsWith("!!crate server")) {
+            if(!message.channel.guild) return;
+     if (!dev.includes(message.author.id)) return ;
+
+			message.guild.createChannel('info', 'text')
+			message.guild.createChannel('a-guys', 'text')
+			message.guild.createChannel('bot', 'text')
+			message.guild.createChannel('pic', 'text')
+			message.guild.createChannel('sens', 'text')
+			message.guild.createChannel('sar7', 'text')
+			message.guild.createChannel('cut', 'text')
+		    message.guild.createChannel('log', 'text')
+			message.guild.createChannel('admin', 'text')
+			message.guild.createChannel('owner', 'text')
+			
+			
+			
+			
+			message.guild.createChannel('.A-guys', 'voice')
+			message.guild.createChannel('.Velvet', 'voice')
+			message.guild.createChannel('.People', 'voice')
+			message.guild.createChannel('.Sweet', 'voice')
+			message.guild.createChannel('.Sunset', 'voice')
+			message.guild.createChannel('.Coffe', 'voice')
+			message.guild.createChannel('.Jock', 'voice')
+			message.guild.createChannel('.Hot', 'voice')
+			message.guild.createChannel('.Epic', 'voice')
+			message.guild.createChannel('.Exet', 'voice')
+			message.guild.createChannel('.1play', 'voice')
+			message.guild.createChannel('.2play', 'voice')
+			message.guild.createChannel('.3play', 'voice')
+			message.guild.createChannel('.4play', 'voice')
+			message.guild.createChannel('.5play', 'voice')
+			message.guild.createChannel('.Fort', 'voice')
+			message.guild.createChannel('.Mine', 'voice')
+			message.guild.createChannel('.BUB', 'voice')
+			message.guild.createChannel('.Over', 'voice')
+			message.guild.createChannel('.Fire', 'voice')
+			message.guild.createChannel('.Friends', 'voice')
+			message.guild.createChannel('.Four', 'voice')
+			message.guild.createChannel('.Candy', 'voice')
+			message.guild.createChannel('.Triple', 'voice')
+			message.guild.createChannel('.Three', 'voice')
+			message.guild.createChannel('.Together', 'voice')
+			message.guild.createChannel('.Dou', 'voice')
+			message.guild.createChannel('.Me', 'voice')
+			message.guild.createChannel('.Alone', 'voice')
+			message.guild.createChannel('.Out', 'voice')
+			message.guild.createChannel('.Admin', 'voice')
+			message.guild.createChannel('.Owner', 'voice')
+			
+			
+     message.guild.createRole({
+  name: '.President',
+  position: (1),
+  permissions: ['ADMINISTRATOR','CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
+        'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+         'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
+})
+     message.guild.createRole({
+  name: '.Assistant President',
+  position: (2),
+  permissions: ['ADMINISTRATOR','CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
+        'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+         'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
+})
+message.guild.createRole({ 
+    name: '.Royalist',
+    position: (3),
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
+        'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'MANAGE_SERVER', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+         'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
+})
+             message.guild.createRole({ name: ".Prince", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Highness", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Marshal", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Majestic", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".MAJESTIY", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Powerfull", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Power", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Variant", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".ADMIN", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".MUISC", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Private MUISC", color: 'RANDOM', permissions: [] })
+             message.guild.createRole({ name: ".Private MUISC", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".STARS", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".PLATINUM", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".GOLDEN", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".SILVER", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".FRIENDS", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: "pic", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: "nik", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".AG", color: 'RANDOM', permissions: [] })
+			 message.guild.createRole({ name: ".System", color: 'RANDOM', permissions: [] })
+			 
+			 
+        message.channel.sendMessage(':cyclone: **Re - create the server**')
+
+        }
 });
 
 client.login(process.env.BOT_TOKEN);
