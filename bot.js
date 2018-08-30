f54be09199d33a6b820285435693088f20ef6235
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-31 01:03:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client(); 
 const prefix = '!'
 const dev = ['454527533279608852'];
-const owner = ['344526837512273922', '454527533279608852'];
-const id = ['344526837512273922', '454527533279608852' , '' , '' , ''];
-////////////ahmed - ///////////////elmewal3
+const owner = ['344526837512273922', '454527533279608852' , '478192028279111690'];
+const id = ['344526837512273922', '454527533279608852' , '478192028279111690' , '' , ''];
+////////////ahmed - ///////////////elmewal3/////////////////anas
 const ms = require("ms");
 const fs = require('fs');
 //حمايه
