7495386db1b615657e23afa40ba610c8c776b579
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-03 19:54:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -602,8 +602,6 @@ client.on('message', message => {
  ╔[❖═════════════════❖]╗
        **Owner Commands**
  ╚[❖═════════════════❖]╝
-
-   **!pbc <user> <message> -** لارسال رساله لشخص الي تمنشنه باستخدام البوت
    
    **!setvoice <room name> -** اذا اتمسح روم الفويز اونلاين تقدر تنشا واحد اخر
   
@@ -908,10 +906,11 @@ if (message.member.voiceChannel != null) {
  var usermentioned = message.mentions.members.first().id;
  var log = message.guild.channels.find('name', 'log');
  var movelog = new Discord.RichEmbed()
-  .setTitle(`<${usermentioned}>`)
+ .setTitle(message.member.username, message.member.avatarURL)
  .setColor("#000000")
  .setDescription(`**<@${usermentioned}> moved to \`\`${message.guild.name}\`\`**`)
- message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(`:white_check_mark:**<@${usermentioned}> moved to \`\`${message.guild.name}\`\`**`))
+ .setTimestamp(message.author.username, message.author.avatarURL);
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(`:white_check_mark: **<@${message.author.id}> moved you to his channel!**`))
  message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => log.send({embed : movelog}))
 	 
 } else {
