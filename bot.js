03198a329d5ccee70153b20f9f8850a12231c6e5
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-07 20:44:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1186,7 +1186,6 @@ if(!message.channel.guild) return;
 if (!message.content.startsWith(prefix)) return;
 	let command = message.content.split(" ")[0];
 	 command = command.slice(prefix.length);
-	let args = message.content.split(" ").slice(1);
 	if (command == "obc") {
 		 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 		var args = message.content.split(' ').slice(1).join(' ');
