4b0a26fe93dc7f49cc0dcc24a2732cef4f495e1b
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-08 21:46:41 +0200

    Update guildMemberAdd.js

--- logs/guildMemberAdd.js
+++ logs/guildMemberAdd.js
@@ -11,7 +11,7 @@ module.exports = (client, member) => {
         .setAuthor(m.username,m.avatarURL)
 	.setDescription(`:arrow_lower_right:<@${member.user.id}> joined the server`)
         .addField(':alarm_clock: Age of account :',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-        .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+        .setFooter(`${m.tag}`, m.avatarURL)
 	.setTimestamp();
 	join.send(embed);          
          
