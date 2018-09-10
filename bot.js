5371c690b5eb1ec6fa128a065a322207bc0e84b3
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-10 21:34:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on('reconnecting', () => console.log('I am reconnecting now!'));
       } else     
         if(message.content === prefix + "restart") {
           if (!dev.includes(message.author.id)) return;
-              message.channel.send(`**Bots restarting** !`);
+            message.channel.send(`:white_check_mark: **Bot restarting** !`);
             console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             console.log(`⚠️ Bot restarting... ⚠️`);
             console.log("===============================================\n\n");
