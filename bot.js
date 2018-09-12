45db441147db22c7821b20221eedf15c1a949371
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-12 23:22:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const { Client } = require('discord.js');
 const client = new Client();
 const prefix = '!'
 const dev = ['454527533279608852'];
-const owner = ['454527533279608852' , '344526837512273922'];
-const id = ['454527533279608852', '344526837512273922' , '' , '' , ''];
-///////////////elmewal3/////////////////a7med
+const owner = ['454527533279608852' , '344526837512273922', '478192028279111690'];
+const id = ['454527533279608852', '344526837512273922' , '478192028279111690' , '' , '', '', '', ''];
+///////////////elmewal3/////////////////a7med////////////////anase
 const ms = require("ms");
 const fs = require('fs');
 //حمايه
@@ -46,10 +46,10 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+  console.log('Yo this ready!')
 });
 client.on('warn', console.warn);
 client.on('error', console.error);
-client.on('ready', () => console.log('Yo this ready!'));
 client.on('reconnecting', () => console.log('I am reconnecting now!'));
 //restart-leve server
       client.on('message', message => {
