68c98253749b2cc4edeaa3191c7a5d2ca528c35f
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-13 23:39:01 +0200

    Create members.js

---
+++ commands/members.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = (client, function(message)) => {
+  if (message.author.bot) return;
+  var prefix = '!'; 
+                  if(!message.channel.guild) return;
+                    if (message.content === prefix + "members") {
+			          if(!message.channel.guild) return; 
+				if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return;
+ const embed = new Discord.RichEmbed()
+    .setDescription(`**Members info ✨
+Online :   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+DND :     ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+Idle :     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
+Offline :     ${message.guild.members.filter(m=>m.presence.status == 'offline').size}
+Server Members :  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} **`)
+         message.channel.send({embed});
+
+    }
+      }
