a312e47c90c1496d3cfce9197a4ced52bc0dcc50
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-14 10:59:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -598,24 +598,7 @@ setInterval(function(){})
 });
 //comand-adminsserver
  //members
-client.on('message', function(message) => {
-  if (message.author.bot) return;
-
-                  if(!message.channel.guild) return;
-                    if (message.content === prefix + "members") {
-			          if(!message.channel.guild) return; 
-				if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return;
- const embed = new Discord.RichEmbed()
-    .setDescription(`**Members info ✨
-Online :   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
-DND :     ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
-Idle :     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
-Offline :     ${message.guild.members.filter(m=>m.presence.status == 'offline').size}
-Server Members :  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} **`)
-         message.channel.send({embed});
-
-    }
-      })
+client.on('message', function message => require('./commands/members.js')(client, function message));
 //mute-unmute
 client.on('message', async message =>{
   if (message.author.boss) return;
