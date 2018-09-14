211e44a9ca4cb59445166134c22dd6e95aedf6ce
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-14 18:39:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,15 +9,12 @@ const id = ['454527533279608852', '344526837512273922' , '478192028279111690' , 
 const ms = require("ms");
 const fs = require('fs');
 //حمايه
-let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
-//warnpac
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
 //idpac
 const Canvas = require("canvas");
 const jimp = require("jimp");
 //profilepac
-const moment = require("moment"); 
-const dateFormat = require('dateformat');
+
 //shortpac
 
 //restartpac
