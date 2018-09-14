bf94746a0d1914d747cb37dbaa7afde63688f75f
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-14 18:37:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,34 +23,7 @@ const dateFormat = require('dateformat');
 //restartpac
 
 //console
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-  console.log('Yo this ready!')
-});
-client.on('warn', console.warn);
-client.on('error', console.error);
-client.on('reconnecting', () => console.log('I am reconnecting now!'));
+
 //restart-leve server
 
 
