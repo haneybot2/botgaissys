9f1d5c4f3eda49e345106ce47a87d204bcb2b813
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-14 15:48:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,71 @@ const prefix = '!'
 const dev = ['454527533279608852'];
 const owner = ['454527533279608852' , '344526837512273922', '478192028279111690'];
 const id = ['454527533279608852', '344526837512273922' , '478192028279111690' , '' , '', '', ''];
-
+///////////////elmewal3/////////////////a7med////////////////anase
+const ms = require("ms");
+const fs = require('fs');
+//حمايه
+let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
+//warnpac
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+//idpac
+const Canvas = require("canvas");
+const jimp = require("jimp");
+//profilepac
+const moment = require("moment"); 
+const dateFormat = require('dateformat');
+//shortpac
+const googl = require('goo.gl');
+//restartpac
+const child_process = require("child_process");
+//console
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+  console.log('Yo this ready!')
+});
+client.on('warn', console.warn);
+client.on('error', console.error);
+client.on('reconnecting', () => console.log('I am reconnecting now!'));
+//restart-leve server
+      client.on('message', message => {
+        var argresult = message.content.split(` `).slice(1).join(' ');
+          if (!dev.includes(message.author.id)) return;
+          
+        if (message.content === (prefix + "levebot")) {
+        message.guild.leave();        
+      } else     
+        if(message.content === prefix + "restart") {
+          if (!dev.includes(message.author.id)) return;
+            message.channel.send(`:white_check_mark: **Bot restarting** !`);
+            console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log(`⚠️ Bot restarting... ⚠️`);
+            console.log("===============================================\n\n");
+            client.destroy();
+            child_process.fork(__dirname + "/bot.js");
+            console.log(`Bot Successfully Restarted`);
+        }
+      
+      });
 
 client.login(process.env.BOT_TOKEN);
