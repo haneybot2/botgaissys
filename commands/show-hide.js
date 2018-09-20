55687328514a3cd8c71e60b972e450f56a567c52
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-20 17:45:53 +0200

    Update show-hide.js

--- commands/show-hide.js
+++ commands/show-hide.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-const owner = ['454527533279608852' , '344526837512273922', '478192028279111690'];
+const id = ['454527533279608852', '344526837512273922' , '478192028279111690' , '' , '', '', '', ''];
 
 
 module.exports = (client, msg) => {
    var prefix = '!!'; 
   if(msg.content === prefix + 'hideall') {
-	  if (!owner.includes(msg.author.id)) return ;
+	  if (!id.includes(msg.author.id)) return ;
     msg.guild.channels.forEach(c => {
       c.overwritePermissions(msg.guild.id, {
         SEND_MESSAGES: false,
@@ -15,7 +15,7 @@ module.exports = (client, msg) => {
     msg.channel.send('Hide All.')
   }
     if(msg.content === prefix + 'showall') {
-	  if (!owner.includes(msg.author.id)) return ;
+	  if (!id.includes(msg.author.id)) return ;
     msg.guild.channels.forEach(c => {
       c.overwritePermissions(msg.guild.id, {
         SEND_MESSAGES: true,
