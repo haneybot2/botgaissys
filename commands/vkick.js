59ed75ffd552c34b0958256aa6b769cfa0ebd12c
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-11-30 14:13:31 +0200

    Update vkick.js

--- commands/vkick.js
+++ commands/vkick.js
@@ -13,13 +13,13 @@ module.exports = (client, message) => {
             return;
         }
 		
-	let vkuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	if(vkuser.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**لا يمكنك طرد احد صوتيا من الاداره**")
-        var member = message.guild.members.get(message.mentions.users.array()[0].id);
         if(!message.mentions.users){
 		message.channel.send(":information_source: ** `!vkick @َζ͜͡ELMEWAL3` يجب تحديد شخص **")
 		return;
         }
+	let vkuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	if(vkuser.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**لا يمكنك طرد احد صوتيا من الاداره**")
+        var member = message.guild.members.get(message.mentions.users.array()[0].id);
 
         if(!member.voiceChannel){
     		message.channel.send("**i can't include voice channel for member!**")
@@ -28,7 +28,6 @@ module.exports = (client, message) => {
               message.guild.createChannel('voicekick', 'voice').then(c => {
                 member.setVoiceChannel(c).then(() => {
                     c.delete(305).catch(console.log)
-
       });
      });
     }
